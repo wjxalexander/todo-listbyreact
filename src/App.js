ae7fe240f4a79a9474f43499091ae699cca3e0d9
wjxalexander <wjxalexander@hotmail.com>
2018-08-24 21:46:28 +0800

    基本框架

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props){
+    super(pros)
+    this.state = {
+      newToDo: 'test',
+      toDoList: [
+        {id: 1, title: '第一个代办'}
+      ]
+    }
+  }
+  render() {
+    let todos = this.state.toDoList.map((item,index)=>{
+      return <li>{item.title}</li>
+    })
+    return (
+      <div className="App">
+        <h1>My to-do list</h1>
+        <div className = 'inputWrapper'>
+        {/*两个注意的点：
+        1. If a tag is empty, you may close it immediately with />, like XML
+        2. Don’t put quotes around curly braces when embedding a JavaScript expression in an attribute. 
+        You should either use quotes (for string values) or curly braces (for expressions), but not both in the same attribute.
+        */}
+          <input type = "text" value ={this.state.newToDo}/>
+        </div>
+        <ol>
+          {todos}
+        </ol>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import Welcome from './Welcome'
+
+ReactDOM.render(
+    <Welcome name = "jingxinwang"/>,
+    //组件不能修改它得到的 props.为什么不能？为了让 bug 更少，这是一种经验，也是 React 推荐的观点。
+    document.getElementById('root')
+)
+registerServiceWorker();
