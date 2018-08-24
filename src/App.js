e47e87b1033ce3947dd9da2c159c3f7ee11cd7b9
wjxalexander <wjxalexander@hotmail.com>
2018-08-24 22:26:36 +0800

    将每个待办封装成 TodoItem

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import TodoInput from '/TodoInput'
+import TodoItem from './TodoItem'
 
 class App extends Component {
   constructor(props){
@@ -8,14 +9,20 @@ class App extends Component {
     this.state = {
       newToDo: 'test',
       toDoList: [
-        {id: 1, title: '第一个代办'}
+        {id: 1, title: '第一个代办'},
+        {id: 2, title: '第一个代办'},
+        {id: 3, title: '第一个代办'},
       ]
     }
   }
   render() {
     let todos = this.state.toDoList.map((item,index)=>{
-      return <li>{item.title}</li>
-    })
+      return (// // 为什么这里要加个括号？这是动手题3 在JS中JavaScript 会自动给行末添加分号。如果 return 后面换行不加括号就会变成 return; 当然不换行一步写完也是可以的，只是难以阅读
+      <li>
+        <TodoItem todo = {item}/>
+      </li>);
+    });
+    
     return (
       <div className="App">
         <h1>My to-do list</h1>
@@ -24,7 +31,6 @@ class App extends Component {
         1. If a tag is empty, you may close it immediately with />, like XML
         2. Don’t put quotes around curly braces when embedding a JavaScript expression in an attribute. 
         You should either use quotes (for string values) or curly braces (for expressions), but not both in the same attribute.
-        
           <input type = "text" value ={this.state.newToDo}/>
         */}
         <TodoInput content ={this.state.newToDo}/>
---
+++ src/TodoItem.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react';
+class TodoItem extends Component{    
+    render(){
+        return <div>{this.props.todo.title}</div>
+    }
+}
+export default TodoItem;
