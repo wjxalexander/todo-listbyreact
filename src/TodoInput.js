78a71c3a7480b0272f938354a9d9f6fd65859a50
wjxalexander <wjxalexander@hotmail.com>
2018-08-25 16:25:22 +0800

    监听回车事件

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -3,7 +3,13 @@ class TodoInput extends Component{
     render(){
         console.log('intodoinput');
         console.log(this.props.content);
-        return <input type = "text" defaultValue ={this.props.content}/>
+        return <input type = "text" defaultValue ={this.props.content}
+            onKeyPress={this.submit}/>
+    }
+    submit(e){
+        if(e.key === 'Enter'){
+            console.log('回车')
+        }
     }
 }
 export default TodoInput;
