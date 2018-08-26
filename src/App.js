440beaa99d3634aea4e5890b698ab4b5d06a1e84
wjxalexander <wjxalexander@hotmail.com>
2018-08-26 14:26:18 +0800

    测试 leancloud 功能

--- src/App.js
+++ src/App.js
@@ -5,6 +5,22 @@ import TodoItem from './TodoItem'
 import 'normalize.css'
 import './reset.css'
 import * as localStore from './localStore'
+import AV from 'leancloud-storage'
+
+var APP_ID = 你自己的ID
+var APP_KEY = 你自己的key
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+})
+
+var TestObject = AV.Object.extend('TestObject')
+var testObject = new TestObject()
+testObject.save({
+  words: 'Hello World!'
+}).then(function(object) {
+  alert('LeanCloud Rocks!')
+})
 
 class App extends Component {
   constructor(props){
